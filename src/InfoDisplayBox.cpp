7290ca68482f9a9c1d6d5cb4815fce06e5b2b6f7
Hanzhi Yin <yinhanzhi@gmail.com>
2022-03-01 08:35:35 -0500

    Make info display box

---
+++ src/InfoDisplayBox.cpp
@@ -0,0 +1,36 @@
+//
+// Created by Hanzhi Yin on 3/1/22.
+//
+
+#include "InfoDisplayBox.h"
+InfoDisplayBox::InfoDisplayBox(const juce::String &componentName)
+    : ListBox(componentName, this) {}
+
+InfoDisplayBox::~InfoDisplayBox() = default;
+
+void InfoDisplayBox::handleAsyncUpdate() {
+  this->updateContent();
+  this->scrollToEnsureRowIsOnscreen(this->getNumRows());
+  this->repaint();
+}
+
+int InfoDisplayBox::getNumRows() { return this->messages.size(); }
+
+void InfoDisplayBox::paintListBoxItem(int rowNumber, juce::Graphics &g,
+                                      int width, int height,
+                                      bool rowIsSelected) {
+  if (rowNumber > this->getNumRows()) {
+    return;
+  }
+
+  g.setColour(juce::Colours::white);
+  g.setFont(15.0);
+  auto textBound = juce::Rectangle<int>(4, 0, width, height);
+  g.drawText(this->messages[rowNumber], textBound,
+             juce::Justification::centredLeft);
+}
+
+void InfoDisplayBox::addMessage(const juce::String &message) {
+  this->messages.add(juce::String(message));
+  this->triggerAsyncUpdate();
+}
