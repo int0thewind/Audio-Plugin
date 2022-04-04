98cbfc526744913ab23f203be3873e8cbdcb0839
Hanzhi Yin <yinhanzhi@gmail.com>
2022-04-03 21:30:53 -0400

    fix np rng lambda

--- prototype/velvetnoise.py
+++ prototype/velvetnoise.py
@@ -5,20 +5,26 @@ import numpy.typing as npt
 
 
 def _energy_decay(alpha: float, number_of_impulses: int) -> float:
+    """
+    Given the alpha value and the number of impulses, what is the target decay?
+    L_db = 20 log_10 (exp(-alpha M))
+    """
     return 20.0 * np.log10(np.exp(-alpha * number_of_impulses))
 
 
 def _inverse_energy_decay(target_decay: float, number_of_impulses: int) -> float:
+    """
+    Given the energy decay and the number of impulses, what is the alpha?
+    """
     return (-1 / number_of_impulses) * np.log(np.power(10, target_decay / 20))
 
 
 def _make_decaying_sample(size: int, alpha: float) -> npt.NDArray[np.float32]:
     rng = np.random.default_rng()
-    return np.fromfunction(
-        lambda m: np.exp(-alpha * m) * 1 * rng.uniform(0.5, 2),
-        shape=(size,),
-        dtype=np.float32,
-    )
+    samples = np.zeros((size,), dtype=np.float32)
+    for i in range(size):
+        samples[i] = np.exp(-alpha * i) * rng.choice([1, -1]) # * rng.uniform(0.5, 2)
+    return samples
 
 
 def velvet_noise(sample_rate: Real,
@@ -29,9 +35,9 @@ def velvet_noise(sample_rate: Real,
     Create a velvet noise discrete-time signal.
 
     :param sample_rate: the sample rate of the returned signal
-    :param filter_length_ms: the total time of the returned signal in milliseconds
+    :param filter_length_ms: the total milliseconds of the returned signal
     :param number_of_impulses: the number of non-zero samples in the returned noise
-    :param target_decay: the amount of decay applied on non-zero samples.
+    :param target_decay: the amount of decay applied on non-zero samples
     :return: a velvet noise. The returned data bit-rate is 32-bit
     """
 
@@ -47,19 +53,13 @@ def velvet_noise(sample_rate: Real,
 
     rng = np.random.default_rng()
     filter_size = np.int32(sample_rate * filter_length_ms / 1000)
-    ret = np.zeros((filter_size,), dtype=np.float32)
+
+    velvet_noise_return = np.zeros((filter_size,), dtype=np.float32)
+
     alpha = _inverse_energy_decay(target_decay, number_of_impulses)
 
     indices = np.sort(rng.choice(filter_size, size=(number_of_impulses,), replace=False, shuffle=False))
     decaying_sample = _make_decaying_sample(number_of_impulses, alpha)
-    ret[indices] = decaying_sample
+    velvet_noise_return[indices] = decaying_sample
 
-    return ret
-
-
-def main():
-    vn = velvet_noise(44100, 30, 8, -20)
-
-
-if __name__ == '__main__':
-    main()
+    return velvet_noise_return
