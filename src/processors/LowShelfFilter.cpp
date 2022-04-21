10a795519366075413e845be9547b61eda1e6823
Hanzhi Yin <yinhanzhi@gmail.com>
2022-04-20 20:30:58 -0400

    fix db to gain transformation

--- src/processors/LowShelfFilter.cpp
+++ src/processors/LowShelfFilter.cpp
@@ -48,9 +48,9 @@ LowShelfFilter::LowShelfFilter(float _cutoffFreq, float _attenuationDecibel)
 
 void LowShelfFilter::updateProcessorSpec() {
   dlog("LowShelfFilter::updateProcessorSpec() method called.");
-  float gain = exp(10 * this->attenuationDecibel);
   this->lowShelf.state = IIRCoefficient::makeLowShelf(
-      this->savedSampleRate, this->cutoffFreq, 5, gain);
+      this->savedSampleRate, this->cutoffFreq, 5,
+      juce::Decibels::decibelsToGain(this->attenuationDecibel));
   this->isDirty = false;
   dlog("LowShelfFilter::updateProcessorSpec() new filter created.");
 }
